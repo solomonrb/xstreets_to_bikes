835aa2e202f6ea800704fcf8d9d5cb542e2e09eb
dakingsol <solomonrb@alumni.brown.edu>
2023-12-01 22:13:31 -0500

    texting front end add

--- app.py
+++ app.py
@@ -19,30 +19,33 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/sms_to_citibike',methods=['POST'])
+@app.route('/sms_to_citibike',methods=['POST','GET'])
 def sms_to_citibike():
-    address = request.form['Body']
-    number = int(request.form['From'].replace("+", ""))
-    current_dt = datetime.now(pytz.timezone('America/New_York')).strftime("%Y-%m-%d %H:%M:%S")
-    
-    target = get_curr_lat_long(address)
+    if request.method == 'POST':
+        address = request.form['Body']
+        number = int(request.form['From'].replace("+", ""))
+        current_dt = datetime.now(pytz.timezone('America/New_York')).strftime("%Y-%m-%d %H:%M:%S")
+        
+        target = get_curr_lat_long(address)
 
-    if target != []:
-        indexed_target = target[0]['geometry']['location']
-        stations = get_station_locations()
-        low_three = find_three_closest(indexed_target, stations)
-        pre_string = get_low_three_status(low_three)
-        converted = convert_to_string(pre_string)
+        if target != []:
+            indexed_target = target[0]['geometry']['location']
+            stations = get_station_locations()
+            low_three = find_three_closest(indexed_target, stations)
+            pre_string = get_low_three_status(low_three)
+            converted = convert_to_string(pre_string)
+        else:
+            converted = "No Google Maps address found. Try cleaning up formatting like 'E 29 St and 1 Av Manhattan' or 'Houston St and Macdougal St' or 'N 7 St and Bedford Av Williamsburg' with no extra words."
+
+        #trigger in background to speed up response
+        executor.submit(db_update_users(number))
+        executor.submit(db_update_events(number,current_dt,address,converted))
+
+        resp = MessagingResponse()
+        resp.message(converted)
+        return str(resp)
     else:
-        converted = "No Google Maps address found. Try cleaning up formatting like 'E 29 St and 1 Av Manhattan' or 'Houston St and Macdougal St' or 'N 7 St and Bedford Av Williamsburg' with no extra words."
-
-    #trigger in background to speed up response
-    executor.submit(db_update_users(number))
-    executor.submit(db_update_events(number,current_dt,address,converted))
-
-    resp = MessagingResponse()
-    resp.message(converted)
-    return str(resp)
+        return "Let's see how this works."
  
 
 if __name__ == "__main__":
